ccce5530a88549004f246d9630006e28966587ce
Ganesh Sonawane <ganeshsonawane4279@gmail.com>
2022-08-16 15:37:34 +0530

    newcode1

---
+++ withnpm/app.js
@@ -0,0 +1,10 @@
+//Import the http module to create the server
+const http = require('http');
+
+
+//local moduels
+const route= require('./route')
+
+const server =  http.createServer(route)
+
+server.listen(4000)
